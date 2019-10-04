a1cf5bec1da45d227c6b76bad315ca948d2c5c83
Athelia Crosmun <athelia.hk@gmail.com>
2019-10-04 02:55:36 +0000

    Commented my function

--- accounting.py
+++ accounting.py
@@ -5,26 +5,45 @@ def identify_wrong_customer_accounts(file_path, melon_cost):
   when the expected payment for a customer does not match actual payment.
 
   """
+  
+# open the file at the path passed in as an argument
   file = open(file_path)
+
+  #iterate through every line and format:
   for line in file:
+    #remove trailing whitespace e.g. newline (superfluous this time)
     line = line.rstrip()
+
+    #split the line into pieces at the divider character
     sections = line.split('|')
     
+    #assign the various sections to more identifiable variables
     customer = sections[1]
-    melons = int(sections[2])
-    paid = float(sections[3])
 
-    amount_due = melon_cost * melons
+    #melons are always sold whole, thus int; but must be cast to number 
+    #to do math below
+    qty_melons = int(sections[2])
+    #amount_paid is cast to float to support decimal differences during 
+    #calculation
+    amount_paid = float(sections[3])
 
+    #how many melons from above and cost of melons (provided as an argument)
+    amount_due = melon_cost * qty_melons
+
+    #check to see if the amounts match
     if amount_due != paid:
+      #if they don't, print a message to highlight the difference
       print("{}".format(customer),
-        "paid ${:.2f},".format(paid),
+        "paid ${:.2f},".format(amount_paid),
         "expected ${:.2f}.".format(amount_due)
         )
 
+  #close the file
   file.close()
 
-identify_wrong_customer_accounts('customer-orders.txt', 1.00)
+#call the function using 1.00 as the melon cost
+identify_wrong_customer_accounts(file_path = 'customer-orders.txt', 
+  melon_cost = 1.00)
 
 """
 #set the melon cost
